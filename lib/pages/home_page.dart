3a4a967f049634ff88ba37e3577ccc2de2055fb0
mglgnzlz <mglgnzlz13@gmail.com>
2023-10-31 15:41:01 +0800

    Authentication is detected, can post messages; detected in Firestore.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:letmecook/assets/icons/logos.dart';
@@ -20,22 +21,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-
   //Variables
 
   // Username display (uncomment as needed)
-  //final currentUser = FirebaseAuth.instance.currentUser;
+  final currentUser = FirebaseAuth.instance.currentUser;
   final textController = TextEditingController();
-
-
-
 
   //FUNCTIONS
   void postMessage() {
     if (textController.text.isNotEmpty) {
       FirebaseFirestore.instance.collection("User Posts").add({
-        //'UserEmail': 'Email@example.com',
+        'UserEmail': currentUser!.email,
         'Message': textController.text,
         'TimeStamp': Timestamp.now(),
       });
@@ -56,8 +52,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-     //bottomNavigationBar: botNav,
+      //bottomNavigationBar: botNav,
 
       backgroundColor: Colors.grey[300],
       //appBar: _appBar,
@@ -67,65 +62,60 @@ class _HomePageState extends State<HomePage> {
         child: Container(
           child: Column(
             children: [
-              // Padding(
-              //   padding: const EdgeInsets.only(top: 10, left: 25, right: 25),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, left: 25, right: 25),
 
-              //   //BOX DECORATION
-              //   child: Container(
+                //BOX DECORATION
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(25),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
+                      )
+                    ],
+                  ),
 
-              //     decoration: BoxDecoration(
-              //       color: Colors.white,
-              //       borderRadius: BorderRadius.circular(25),
-              //       boxShadow: [
-              //         BoxShadow(
-              //          color: Colors.grey.withOpacity(0.5),
-              //          spreadRadius: 5,
-              //          blurRadius: 7,
-              //          offset: Offset(0, 3),
-              //         )
-              //       ],
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  padding: const EdgeInsets.all(15),
 
-              //     ),
+                  // PROFILE PICTURE
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.grey[400]),
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.white,
+                        ),
+                      ),
+                      // POST MESSAGE
+                      Expanded(
+                          child: MyTextField(
+                        minLines: 1,
+                        maxLines: 150,
+                        controller: textController,
+                        hintText: 'Post a new recipe!',
+                        obscureText: false,
+                      )),
 
-              //     margin: const EdgeInsets.symmetric(vertical: 10),
-              //     padding: const EdgeInsets.all(15),
-
-              //   // PROFILE PICTURE
-              //     child: Row(
-              //       children: [
-              //         Container(
-              //           decoration: BoxDecoration(
-              //               shape: BoxShape.circle, color: Colors.grey[400]),
-              //           padding: EdgeInsets.all(10),
-
-              //           child: Icon(
-              //             Icons.person,
-              //             color: Colors.white,
-              //           ),
-              //         ),
-              //   // POST MESSAGE
-              //         Expanded(
-              //             child: MyTextField(
-              //           minLines: 1,
-              //           maxLines: 150,
-              //           controller: textController,
-              //           hintText: 'Post a new recipe!',
-              //           obscureText: false,
-
-              //         )
-              //         ),
-
-              //         // POST BUTTON
-              //         IconButton(
-              //             onPressed: postMessage,
-              //             icon: const Icon(Icons.arrow_circle_up)),
-              //         IconButton(
-              //           onPressed: attachImage,
-              //           icon: const Icon(Icons.camera_alt_rounded))
-              //       ],
-              //     ),
-              //   ),
-              // ),
+                      // POST BUTTON
+                      IconButton(
+                          onPressed: postMessage,
+                          icon: const Icon(Icons.arrow_circle_up)),
+                      IconButton(
+                          onPressed: attachImage,
+                          icon: const Icon(Icons.camera_alt_rounded))
+                    ],
+                  ),
+                ),
+              ),
 
               // Wall Display (boxes)
               Expanded(
@@ -145,7 +135,7 @@ class _HomePageState extends State<HomePage> {
                             final post = snapshot.data!.docs[index];
                             return WallPost(
                               message: post['Message'],
-                              //user: post['UserEmail'],
+                              user: post['UserEmail'],
                             );
                           }));
                     } else if (snapshot.hasError) {
@@ -161,7 +151,7 @@ class _HomePageState extends State<HomePage> {
               ),
               // Logged in as : section
 
-              //Text("Logged in as : " + currentUser.email )
+              Text("Logged in as : ${currentUser!.email}")
             ],
           ),
         ),
