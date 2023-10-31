826ad8c8eddfa0384ba20fca3d33fe009ba6795c
Daniel John Cedric Macallan <cedricmacallan@gmail.com>
2023-10-31 16:46:59 +0800

    Created profile user display

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,18 +1,81 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:letmecook/assets/icons/logos.dart';
+import 'package:letmecook/assets/themes/app_colors.dart';
+import 'package:letmecook/pages/post_page.dart';
+import 'package:letmecook/pages/profile_page.dart';
+import 'package:letmecook/pages/search_page.dart';
+import 'package:letmecook/widgets/styled_text.dart';
+import 'package:letmecook/widgets/textField.dart';
+import 'package:letmecook/assets/icons/custom_icons.dart';
+import 'package:letmecook/widgets/wall_post.dart';
 import 'package:letmecook/widgets/topAppBar.dart';
 
-
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final currentUser = FirebaseAuth.instance.currentUser;
+  @override
   Widget build(BuildContext context) {
-    final topAppBar = myAppBar();
-
-
     return Scaffold(
-
-      body: Text('Profile'),
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10, left: 25, right: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(25),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3),
+                      )
+                    ],
+                  ),
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.grey[400]),
+                        padding: EdgeInsets.all(10),
+                        child: const Icon(
+                          Icons.person,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          child: StyledText(
+                            text: '${currentUser!.email}',
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () {}, icon: const Icon(Icons.settings)),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
