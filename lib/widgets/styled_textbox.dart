4abc58ff671d91b15841e870134325080e182615
clarence00 <clarencenativdad12@gmail.com>
2023-11-02 02:00:29 +0800

    [mod]: poppins, min, and max lined added

--- lib/widgets/styled_textbox.dart
+++ lib/widgets/styled_textbox.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:letmecook/assets/themes/app_colors.dart';
 
 class StyledTextbox extends StatelessWidget {
@@ -8,6 +9,8 @@ class StyledTextbox extends StatelessWidget {
       this.text = '',
       this.hintText = '',
       this.height = 40,
+      this.minLines = 1,
+      this.maxLines = 1,
       required this.controller})
       : super(key: key);
 
@@ -16,6 +19,8 @@ class StyledTextbox extends StatelessWidget {
   final String text;
   final String hintText;
   final double height;
+  final int minLines;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
@@ -23,14 +28,20 @@ class StyledTextbox extends StatelessWidget {
 
     return Container(
       height: height,
-      margin: const EdgeInsets.only(top: 10, bottom: 10),
+      // margin: const EdgeInsets.only(top: 10, bottom: 10),
       decoration: ShapeDecoration(
         color: AppColors.background,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
       child: TextField(
+        minLines: minLines,
+        maxLines: maxLines,
         controller: controller,
-        style: const TextStyle(fontSize: 16),
+        style: GoogleFonts.poppins(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          color: AppColors.dark,
+        ),
         obscureText: obscureText,
         decoration: InputDecoration(
           contentPadding: const EdgeInsets.only(left: 10, right: 10, bottom: 5),
