83b09c3e6ba8cfcb7dd81f92c679f2c1b9452189
clarence00 <clarencenatividad12@gmail.com>
2023-11-01 12:10:25 +0800

    [ref]: Refactored filenames

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:letmecook/widgets/topAppBar.dart';
-
+import 'package:letmecook/widgets/top_appbar.dart';
 
 class PostPage extends StatelessWidget {
   const PostPage({super.key});
 
   @override
   Widget build(BuildContext context) {
- 
-
     return Scaffold(
-
       body: Text('Post Something'),
     );
   }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -9,10 +9,10 @@ import 'package:letmecook/pages/profile_page.dart';
 import 'package:letmecook/pages/search_page.dart';
 import 'package:letmecook/widgets/styled_text.dart';
 import 'package:letmecook/widgets/styled_textbox.dart';
-import 'package:letmecook/widgets/textField.dart';
+import 'package:letmecook/widgets/text_field.dart';
 import 'package:letmecook/assets/icons/custom_icons.dart';
 import 'package:letmecook/widgets/wall_post.dart';
-import 'package:letmecook/widgets/topAppBar.dart';
+import 'package:letmecook/widgets/top_appbar.dart';
 import 'package:letmecook/auth.dart';
 
 class ProfilePage extends StatefulWidget {
