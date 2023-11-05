8adcd55fc0024d5d3b51af56a68c0ed7a566a2f4
clarence00 <clarencenativdad12@gmail.com>
2023-11-06 00:58:36 +0800

    [mod]: serach page initial ui

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:letmecook/widgets/top_appbar.dart';
+import 'package:letmecook/assets/themes/app_colors.dart';
+import 'package:letmecook/widgets/styled_container.dart';
+import 'package:letmecook/widgets/styled_text.dart';
+import 'package:letmecook/widgets/styled_textbox.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final topAppBar = MyAppBar();
+  State<SearchPage> createState() => _SearchPageState();
+}
 
+// Variable for UI only (should be changed accordingly)
+final _controllerSearch = TextEditingController();
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Search'),
+      backgroundColor: AppColors.background,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            StyledContainer(
+              child: StyledTextbox(
+                controller: _controllerSearch,
+                hintText: 'Search',
+              ),
+            ),
+            const StyledContainer(
+                child: StyledText(
+              text: 'Basta mga tao ito',
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
