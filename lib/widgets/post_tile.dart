b2423dca24769ee22165cb834ffee4ae8d346c5e
clarence00 <clarencenativdad12@gmail.com>
2023-11-06 00:59:28 +0800

    [add]: viewpost routing added

--- lib/widgets/post_tile.dart
+++ lib/widgets/post_tile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:letmecook/assets/icons/custom_icons.dart';
+import 'package:letmecook/pages/viewpost_post.dart';
 import 'package:letmecook/widgets/styled_text.dart';
 import 'package:letmecook/assets/themes/app_colors.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -35,6 +35,11 @@ class _PostTileState extends State<PostTile> {
     fetchUserData();
   }
 
+  void toViewPost() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const ViewPostPage()));
+  }
+
   void fetchUserData() async {
     final snapshot = await FirebaseFirestore.instance
         .collection('Usernames')
@@ -66,142 +71,147 @@ class _PostTileState extends State<PostTile> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 16, right: 16, top: 10),
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-      decoration: BoxDecoration(
-        color: AppColors.light,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: AppColors.dark.withOpacity(0.25),
-            spreadRadius: 0,
-            blurRadius: 15,
-            offset: const Offset(0, 5),
-          )
-        ],
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // First Div (Profile)
-              Container(
-                margin: const EdgeInsets.only(right: 5),
-                child: profilePictureUrl != ''
-                    ? CircleAvatar(
-                        radius: 16,
-                        backgroundImage: NetworkImage(profilePictureUrl),
-                      )
-                    : const CircleAvatar(
-                        radius: 16,
-                        backgroundColor: AppColors.light,
-                        child: CircularProgressIndicator(),
+    return GestureDetector(
+      onTap: () {
+        toViewPost();
+      },
+      child: Container(
+        margin: const EdgeInsets.only(left: 16, right: 16, top: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        decoration: BoxDecoration(
+          color: AppColors.light,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.dark.withOpacity(0.25),
+              spreadRadius: 0,
+              blurRadius: 15,
+              offset: const Offset(0, 5),
+            )
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // First Div (Profile)
+                Container(
+                  margin: const EdgeInsets.only(right: 5),
+                  child: profilePictureUrl != ''
+                      ? CircleAvatar(
+                          radius: 16,
+                          backgroundImage: NetworkImage(profilePictureUrl),
+                        )
+                      : const CircleAvatar(
+                          radius: 16,
+                          backgroundColor: AppColors.light,
+                          child: CircularProgressIndicator(),
+                        ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Flexible(
+                        child: StyledText(
+                          text: username,
+                        ),
                       ),
-              ),
-              Expanded(
-                child: Row(
-                  children: [
-                    Flexible(
-                      child: StyledText(
-                        text: username,
+                      const Padding(
+                        padding: EdgeInsets.only(left: 5, right: 5),
+                        child: Icon(
+                          Icons.circle_rounded,
+                          color: AppColors.accent,
+                          size: 8,
+                        ),
                       ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 5, right: 5),
-                      child: Icon(
-                        Icons.circle_rounded,
+                      StyledText(
+                        text: getPostTimeDisplay(widget.timestamp),
+                        size: 12,
                         color: AppColors.accent,
-                        size: 8,
                       ),
+                    ],
+                  ),
+                ),
+                const Icon(
+                  Icons.more_vert,
+                  color: AppColors.dark,
+                  size: 24,
+                ),
+              ],
+            ),
+            // Second Div (Title)
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 5),
+              alignment: Alignment.centerLeft,
+              child: StyledText(
+                text: widget.post,
+                size: 20,
+                weight: FontWeight.w700,
+              ),
+            ),
+            // Third Div (Image)
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.network(
+                  'https://picsum.photos/id/1074/400/400',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            // Fourth Div (Reacts)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: CustomIcons.heart(color: _heartColor),
                     ),
-                    StyledText(
-                      text: getPostTimeDisplay(widget.timestamp),
-                      size: 12,
-                      color: AppColors.accent,
+                    Container(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: const StyledText(
+                        text: '12',
+                      ),
                     ),
                   ],
                 ),
-              ),
-              const Icon(
-                Icons.more_vert,
-                color: AppColors.dark,
-                size: 24,
-              ),
-            ],
-          ),
-          // Second Div (Title)
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 5),
-            alignment: Alignment.centerLeft,
-            child: StyledText(
-              text: widget.post,
-              size: 20,
-              weight: FontWeight.w700,
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: CustomIcons.comment(color: _heartColor),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: const StyledText(
+                        text: '12',
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: CustomIcons.bookmark(color: _heartColor),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: const StyledText(
+                        text: '12',
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ),
-          // Third Div (Image)
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.network(
-                'https://picsum.photos/id/1074/400/400',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          // Fourth Div (Reacts)
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: CustomIcons.heart(color: _heartColor),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.only(right: 12),
-                    child: const StyledText(
-                      text: '12',
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: CustomIcons.comment(color: _heartColor),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.only(right: 12),
-                    child: const StyledText(
-                      text: '12',
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: CustomIcons.bookmark(color: _heartColor),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.only(right: 12),
-                    child: const StyledText(
-                      text: '12',
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
