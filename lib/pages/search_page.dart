52c8ec1166e50a6c6f0699668cc41e2ecbee49c9
mglgnzlz <mglgnzlz13@gmail.com>
2023-12-03 15:24:00 +0800

    [feature] : Search page is functioning as intended

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:letmecook/assets/themes/app_colors.dart';
 import 'package:letmecook/widgets/styled_container.dart';
@@ -15,26 +16,107 @@ class SearchPage extends StatefulWidget {
 final _controllerSearch = TextEditingController();
 
 class _SearchPageState extends State<SearchPage> {
+  String Username = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.background,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            StyledContainer(
-              child: StyledTextbox(
-                controller: _controllerSearch,
-                hintText: 'Search',
-              ),
-            ),
-            const StyledContainer(
-                child: StyledText(
-              text: 'Basta mga tao ito',
-            ))
-          ],
-        ),
-      ),
-    );
+        appBar: AppBar(
+            title: Card(
+          child: Column(
+            children: [
+              StyledContainer(
+                  child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  hintText: 'Search...',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    Username = value;
+                  });
+                },
+              )),
+            ],
+          ),
+        )),
+        backgroundColor: AppColors.background,
+        body: StreamBuilder<QuerySnapshot>(
+          stream:
+              FirebaseFirestore.instance.collection('Usernames').snapshots(),
+          builder: (context, snapshots) {
+            return (snapshots.connectionState == ConnectionState.waiting)
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemCount: snapshots.data!.docs.length,
+                    itemBuilder: ((context, index) {
+                      var data = snapshots.data!.docs[index].data()
+                          as Map<String, dynamic>;
+
+                      if (Username.isEmpty) {
+                        return ListTile(
+                          title: Text(
+                            data['Username'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            data['UserEmail'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(data['ProfilePicture']),
+                          ),
+                        );
+                      }
+
+                      if (data['Username']
+                          .toString()
+                          .startsWith(Username.toLowerCase())) {
+                        return ListTile(
+                          title: Text(
+                            data['Username'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            data['UserEmail'],
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(data['ProfilePicture']),
+                          ),
+                        );
+                      }
+                      return Container();
+                    }));
+          },
+        ));
   }
 }
