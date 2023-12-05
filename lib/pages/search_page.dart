7a7e2a8023ea0d9185cfe99abce002ddfbf7e982
clarence00 <clarencenativdad12@gmail.com>
2023-12-06 01:12:31 +0800

    [ui]: search page ui modified

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:letmecook/assets/themes/app_colors.dart';
-import 'package:letmecook/widgets/styled_container.dart';
+import 'package:letmecook/pages/profile_page.dart';
 import 'package:letmecook/widgets/styled_text.dart';
-import 'package:letmecook/widgets/styled_textbox.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -13,110 +13,151 @@ class SearchPage extends StatefulWidget {
 }
 
 // Variable for UI only (should be changed accordingly)
-final _controllerSearch = TextEditingController();
 
 class _SearchPageState extends State<SearchPage> {
-  String Username = "";
+  String username = "";
+
+  void toProfile() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const ProfilePage()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Card(
-          child: Column(
-            children: [
-              StyledContainer(
-                  child: TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search),
+      backgroundColor: AppColors.background,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            decoration: BoxDecoration(
+              color: AppColors.light,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(24),
+                bottomRight: Radius.circular(24),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.dark.withOpacity(0.25),
+                  spreadRadius: 0,
+                  blurRadius: 15,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: AppColors.background,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: TextField(
+                style: GoogleFonts.poppins(
+                  color: AppColors.dark,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search_rounded,
+                    color: AppColors.dark,
+                    size: 24,
+                  ),
                   hintText: 'Search...',
+                  border: InputBorder.none,
                 ),
                 onChanged: (value) {
-                  setState(() {
-                    Username = value;
-                  });
+                  setState(
+                    () {
+                      username = value;
+                    },
+                  );
                 },
-              )),
-            ],
+              ),
+            ),
           ),
-        )),
-        backgroundColor: AppColors.background,
-        body: StreamBuilder<QuerySnapshot>(
-          stream:
-              FirebaseFirestore.instance.collection('Usernames').snapshots(),
-          builder: (context, snapshots) {
-            return (snapshots.connectionState == ConnectionState.waiting)
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : ListView.builder(
-                    itemCount: snapshots.data!.docs.length,
-                    itemBuilder: ((context, index) {
-                      var data = snapshots.data!.docs[index].data()
-                          as Map<String, dynamic>;
+          const SizedBox(height: 15),
+          const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: StyledText(
+              text: 'Recent',
+              size: 20,
+            ),
+          ),
+          const SizedBox(height: 10),
 
-                      if (Username.isEmpty) {
-                        return ListTile(
-                          title: Text(
-                            data['Username'],
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          subtitle: Text(
-                            data['UserEmail'],
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          leading: CircleAvatar(
-                            backgroundImage:
-                                NetworkImage(data['ProfilePicture']),
-                          ),
-                        );
-                      }
+          // People
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('Usernames')
+                  .snapshots(),
+              builder: (context, snapshots) {
+                return (snapshots.connectionState == ConnectionState.waiting)
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : ListView.builder(
+                        itemCount: snapshots.data!.docs.length,
+                        itemBuilder: ((context, index) {
+                          var data = snapshots.data!.docs[index].data()
+                              as Map<String, dynamic>;
 
-                      if (data['Username']
-                          .toString()
-                          .startsWith(Username.toLowerCase())) {
-                        return ListTile(
-                          title: Text(
-                            data['Username'],
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          subtitle: Text(
-                            data['UserEmail'],
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          leading: CircleAvatar(
-                            backgroundImage:
-                                NetworkImage(data['ProfilePicture']),
-                          ),
-                        );
-                      }
-                      return Container();
-                    }));
-          },
-        ));
+                          if (username.isEmpty ||
+                              data['Username']
+                                  .toString()
+                                  .startsWith(username.toLowerCase())) {
+                            return GestureDetector(
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 5),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 15, vertical: 10),
+                                decoration: BoxDecoration(
+                                  color: AppColors.light,
+                                  borderRadius: BorderRadius.circular(16),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: AppColors.dark.withOpacity(0.25),
+                                      spreadRadius: 0,
+                                      blurRadius: 15,
+                                      offset: const Offset(0, 5),
+                                    ),
+                                  ],
+                                ),
+                                child: Row(
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundImage:
+                                          NetworkImage(data['ProfilePicture']),
+                                      radius: 25,
+                                    ),
+                                    const SizedBox(width: 15),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        StyledText(text: data['Username']),
+                                        StyledText(text: data['UserEmail']),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              onTap: () => toProfile(),
+                            );
+                          }
+                          return Container();
+                        }),
+                      );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
